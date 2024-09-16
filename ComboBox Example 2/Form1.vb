a3c094b6d49769a97137d6e3d345e495194d5c46
Manobendra <manovmandal@gmail.com>
2024-09-16 19:49:58 +0530

    Revised ComboBox and ListBox examples

---
+++ ComboBox Example 2/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim items() As String = {"C", "C++", "Java", "Python", "Rust", "Javascript",
+           "Solidity", "MongoDB", "GoLang", "SQL"}
+
+        ComboBox1.Items.AddRange(items)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Add(ComboBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim itemsArray(ListBox1.Items.Count - 1) As String
+        ListBox1.Items.CopyTo(itemsArray, 0)
+        ListBox1.Items.Clear()
+        Array.Sort(itemsArray)
+        ListBox1.Items.AddRange(itemsArray)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+        ComboBox1.Items.Clear()
+    End Sub
+End Class
---
+++ ListBox/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim cities() As String = {"New York", "Tokyo", "Berlin", "Kathmandu", "Chennai"}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.AddRange(cities)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles count.Click
+        MsgBox("Total cities: " & ListBox1.Items.Count)
+    End Sub
+
+    Private Sub sort_Click(sender As Object, e As EventArgs) Handles sort.Click
+        ' Clear ListBox2 before adding sorted items
+        ListBox2.Items.Clear()
+        Array.Sort(cities)
+        ListBox2.Items.AddRange(cities)
+        MsgBox("Cities sorted and added to ListBox2.")
+    End Sub
+
+    Private Sub remove_Click(sender As Object, e As EventArgs) Handles remove.Click
+        If ListBox1.SelectedItem IsNot Nothing Then
+            Dim selectedItem As String = ListBox1.SelectedItem.ToString()
+            Dim result As DialogResult = MessageBox.Show("Are you sure you want to remove " & selectedItem & "?", "Confirm Removal", MessageBoxButtons.YesNo)
+
+            If result = DialogResult.Yes Then
+                ListBox1.Items.Remove(ListBox1.SelectedItem)
+                MsgBox(selectedItem & " has been removed.")
+            End If
+        Else
+            MsgBox("Please select an item to remove.")
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        sort.Enabled = ListBox1.SelectedItem IsNot Nothing
+        remove.Enabled = ListBox1.SelectedItem IsNot Nothing
+    End Sub
+End Class
